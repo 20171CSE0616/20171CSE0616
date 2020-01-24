e3c571b10925d0fff6e7ada69818f214ac786452
20171CSE0616 <60249742+20171CSE0616@users.noreply.github.com>
2020-01-24 10:55:16 +0530

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char choice;
+printf("enter the choice\n");
+scanf("%c",&choice);
+if(choice=='p')
+printf("physics cycle\n");
+else if(choice=='c')
+printf("chemistry cycle\n");
+else
+printf("not registred\n");
+}
